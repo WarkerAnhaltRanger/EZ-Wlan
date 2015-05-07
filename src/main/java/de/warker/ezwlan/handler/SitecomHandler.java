8702e1808c0d03e4cc75eb8a73f3e919e0cf41cb
Warker <warkeranhaltranger@googlemail.com>
2015-05-07 18:25:29 +0200

    fixed issue #2 thx to ruiaraujo

--- src/main/java/de/warker/ezwlan/handler/SitecomHandler.java
+++ src/main/java/de/warker/ezwlan/handler/SitecomHandler.java
@@ -1,50 +1,60 @@
 package de.warker.ezwlan.handler;
 
 import android.net.wifi.ScanResult;
+import android.util.Log;
 
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 public class SitecomHandler extends AbstractWlanKeyHandler {
 	
 	public SitecomHandler() {
 		SUPPORTED_MACS.add("00:0C:F6");
 		SUPPORTED_SSID.add("Sitecom");
-		
 	}
 	
-	private final String lt = "123456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ"; // without i,l,o,0
+	private final String lt = "123456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ"; // without i,I,l,L,o,O,0
 
 	@Override
 	public String[] getKeys(ScanResult sr) {
-		String wpakey = "";
-		String mac = HandlerFactory.bssid_helper(sr.BSSID).toLowerCase();
-		// atol 
-		int var_80 = 0;
-		String t = "";
-		for(int i = 6; i < mac.length(); ++i){
-			char c = mac.charAt(i);
-			if(c >= '0' && c <= '9'){
-				t+=c;
-			}
-			else{
-				if(t.length() > 0){
-					var_80 = Integer.parseInt(t);
+
+		final List<String> keys = new LinkedList<String>();
+
+		String [] macs = {
+				HandlerFactory.bssid_helper(sr.BSSID).toLowerCase(),	// wlm2500
+				HandlerFactory.bssid_helper(sr.BSSID).toUpperCase()		// wlm3500 thx to Rui Araújo (issue #2)
+				// TODO: wlm5500
+		};
+
+		for(String mac : macs) {
+
+			String wpakey = "";
+			// atol
+			String t = "0";
+			for (int i = 6; i < mac.length(); ++i) {
+				char c = mac.charAt(i);
+				if (c >= '0' && c <= '9') {
+					t += c;
+				} else {
+					break;
 				}
-				break;
 			}
+
+			int var_80 = Integer.parseInt(t);
+
+			// algo
+			wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(5)) * (mac.charAt(9) + mac.charAt(3) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(6)) * (mac.charAt(8) + mac.charAt(10) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(3) + mac.charAt(5)) * (mac.charAt(7) + mac.charAt(9) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(7) + mac.charAt(6)) * (mac.charAt(5) + mac.charAt(4) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(7) + mac.charAt(6)) * (mac.charAt(8) + mac.charAt(9) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(5)) * (mac.charAt(3) + mac.charAt(4) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(4)) * (mac.charAt(6) + mac.charAt(8) + mac.charAt(11))) % lt.length());
+			wpakey += lt.charAt(((var_80 + mac.charAt(10) + mac.charAt(11)) * (mac.charAt(7) + mac.charAt(8) + mac.charAt(11))) % lt.length());
+
+			keys.add(wpakey);
 		}
-		// algo
-		wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(5)) * (mac.charAt(9) + mac.charAt(3) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(6)) * (mac.charAt(8) + mac.charAt(10) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(3) + mac.charAt(5)) * (mac.charAt(7) + mac.charAt(9) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(7) + mac.charAt(6)) * (mac.charAt(5) + mac.charAt(4) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(7) + mac.charAt(6)) * (mac.charAt(8) + mac.charAt(9) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(5)) * (mac.charAt(3) + mac.charAt(4) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(11) + mac.charAt(4)) * (mac.charAt(6) + mac.charAt(8) + mac.charAt(11)))%lt.length());
-		wpakey += lt.charAt(((var_80 + mac.charAt(10) + mac.charAt(11)) * (mac.charAt(7) + mac.charAt(8) + mac.charAt(11)))%lt.length());
-
-		return new String[] { wpakey };
+		return keys.toArray(new String[0]);
 	}
-
 }
